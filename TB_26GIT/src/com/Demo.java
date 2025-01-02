2473b9b11c5718b79dab82864473193dabf1dc1c
iNDIA <iNDIA@DESKTOP-F45EQOO>
2025-01-02 19:43:16 +0530

    by update

--- TB_26GIT/src/com/Demo.java
+++ TB_26GIT/src/com/Demo.java
@@ -1,6 +1,10 @@
 package com;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("This is m1");
+	}
 	public static void main(String args[]) {
 		System.out.println("This is main method");
 	}
