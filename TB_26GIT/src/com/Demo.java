8fd80a5716362d65d8c3fa497172555c7facde87
ap2909 <akanksha290901@gmail.com>
2025-01-02 19:46:05 +0530

    By git
    add new method in our project

--- TB_26GIT/src/com/Demo.java
+++ TB_26GIT/src/com/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
 	{
 		System.out.println("This is m1");
 	}
+	public void m2()
+	{
+		System.out.println("This is m2");
+	}
 	public static void main(String args[]) {
 		System.out.println("This is main method");
 	}
